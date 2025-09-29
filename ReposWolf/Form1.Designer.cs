a8350b7b8e2d782d1edf9d515c8a11e2e13d915d
Wolfy768 <pihtovnikovvaceslav@gmail.com>
2025-09-29 15:04:04 +0500

    Update Form1.Designer.cs

--- ReposWolf/Form1.Designer.cs
+++ ReposWolf/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(275, 123);
+            this.button1.Location = new System.Drawing.Point(281, 169);
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(281, 169);
             this.button1.Name = "button1";
@@ -48,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(275, 239);
+            this.button2.Location = new System.Drawing.Point(281, 261);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(207, 72);
             this.button2.TabIndex = 1;
@@ -92,7 +92,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
     }
