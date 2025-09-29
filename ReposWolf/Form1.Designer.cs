96ab882f3043345e3298643efed16f949c411a2a
Wolfy768 <pihtovnikovvaceslav@gmail.com>
2025-09-29 15:22:54 +0500

    Update Form1.Designer.cs

--- ReposWolf/Form1.Designer.cs
+++ ReposWolf/Form1.Designer.cs
@@ -29,28 +29,15 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-<<<<<<< HEAD
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-=======
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
->>>>>>> 9eb9d1eb780659789ed170bcdef8ba9ef0fe8853
             this.SuspendLayout();
             // 
             // button1
             // 
-<<<<<<< HEAD
-            this.button1.Location = new System.Drawing.Point(275, 123);
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(281, 169);
-=======
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(281, 169);
-            this.button1.Location = new System.Drawing.Point(275, 123);
->>>>>>> 9eb9d1eb780659789ed170bcdef8ba9ef0fe8853
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(207, 86);
             this.button1.TabIndex = 0;
@@ -58,18 +45,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-<<<<<<< HEAD
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(275, 239);
+            this.button2.Location = new System.Drawing.Point(281, 261);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(207, 72);
             this.button2.TabIndex = 1;
             this.button2.Text = "Закрыть";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-=======
->>>>>>> 9eb9d1eb780659789ed170bcdef8ba9ef0fe8853
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -87,33 +71,15 @@
             this.label1.Size = new System.Drawing.Size(263, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Введите текст для вывода";
-<<<<<<< HEAD
-=======
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(275, 239);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(207, 72);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Закрыть";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
->>>>>>> 9eb9d1eb780659789ed170bcdef8ba9ef0fe8853
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-<<<<<<< HEAD
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-=======
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
->>>>>>> 9eb9d1eb780659789ed170bcdef8ba9ef0fe8853
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -125,15 +91,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-<<<<<<< HEAD
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-=======
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
->>>>>>> 9eb9d1eb780659789ed170bcdef8ba9ef0fe8853
+
     }
 }
 
